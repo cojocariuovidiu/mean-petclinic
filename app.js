bd6017588134c815e2d3c361c9c692e1693b89d5
richarae <Anthony.Richardson-Contractor@adp.com>
2014-02-03 09:02:12 -0500

    Initial commit - Project set up work in progress.

---
+++ app.js
@@ -0,0 +1,34 @@
+var express = require('express'),
+    http = require('http'),
+    path = require('path'),
+    cons = require('consolidate'),
+    util = require('util'),
+    app = express();
+
+app.engine('html', cons.handlebars);
+app.set('port', process.env.PORT || 3000);
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+app.use(express.favicon('public/img/favicon.ico'));
+//app.use(express.favicon(path.join(__dirname, 'public/img/favicon.ico')));
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(express.cookieParser('your secret here'));
+app.use(express.session());
+app.use(auth.passport.initialize());
+app.use(auth.passport.session());
+app.use(express.csrf());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(utils.response.redirect('/'));
+if ('development' == app.get('env')) {
+    app.use(express.errorHandler());
+}
+
+app.get('/', routes.home.index);
+
+http.createServer(app).listen(app.get('port'), function () {
+    logger.info('Express server listening on port ' + config.get('express:port'));
+});

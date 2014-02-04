aba04720380b218099b0192ae86791eae1a4c4ce
richarae <anthony.richardson-contractor@adp.com>
2014-02-04 08:06:39 -0500

    Initial commit

--- app.js
+++ app.js
@@ -15,9 +15,6 @@ app.engine('html', cons.handlebars);
 app.set('port', process.env.PORT || 3000);
 app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
-//TODO: Why doesn't this work?
-//app.use(express.favicon('img/favicon.ico'));
-//app.use(express.favicon(path.join(__dirname, 'public/img/favicon.ico')));
 app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
@@ -30,15 +27,11 @@ app.use(express.csrf());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(utils.response.redirect('/'));
-if ('development' == app.get('env')) {
+if ('development' === app.get('env')) {
     app.use(express.errorHandler());
 }
 
 app.get('/', routes.home.index);
-app.get('/favicon.ico', function (req, res) {
-    res.writeHead(200, {'Content-Type': 'image/x-icon'});
-    res.end();
-});
 
 http.createServer(app).listen(app.get('port'), function () {
     logger.info('Express server listening on port ' + config.get('express:port'));

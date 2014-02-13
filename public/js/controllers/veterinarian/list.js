1f6bc0a0de84ca38bf1d4bd43e6e5a1a2aa18140
anri3534 <anthony.glenn.richardson@gmail.com>
2014-02-13 15:27:52 -0500

    Vet sort and filter feature.

--- public/js/controllers/veterinarian/list.js
+++ public/js/controllers/veterinarian/list.js
@@ -3,8 +3,6 @@
 var petClinic = petClinic || {};
 
 petClinic.controller('vetList', function ($scope, vetService) {
-    $scope.listFailure = false;
-
     vetService.findAll(function (err, vets) {
         $scope.listFalure = false;
         if (err) {
@@ -13,4 +11,28 @@ petClinic.controller('vetList', function ($scope, vetService) {
             $scope.vets = vets;
         }
     });
+
+    $scope.listFailure = false;
+    $scope.sortOrder = 'lastName';
+    $scope.searchTerm = '';
+
+    $scope.search = function (searchTerm) {
+        $scope.searchTerm = searchTerm;
+    }
+
+    $scope.sortByName = function () {
+        if ($scope.sortOrder === 'lastName') {
+            $scope.sortOrder = '-lastName';
+        } else {
+            $scope.sortOrder = 'lastName';
+        }
+    };
+
+    $scope.sortBySpecialty = function () {
+        if ($scope.sortOrder === 'specialty.name') {
+            $scope.sortOrder = '-specialty.name';
+        } else {
+            $scope.sortOrder = 'specialty.name';
+        }
+    };
 });
